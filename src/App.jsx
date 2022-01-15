afaae9ef926c131df54ba021a184fa196007d8a0
Ben Aepli <baepli@jhcschool.org>
2022-01-15 16:13:27 -0700

    fixed styling, removed power and volume sliders

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
 import soundFiles from './soundFiles';
 import useComponentWillMount from "./useComponentWillMount";
 
 const App = () => {
-    const [power, setPower] = useState(false);
     const [sound, setSound] = useState("");
-    const [volume, setVolume] = useState(50);
-    const [style] = useState(Array(9).fill({ "backgroundColor": "hsl(0, 0%, 15%)" }));
+    const [style, setStyle] = useState(Array(9).fill({ "backgroundColor": "hsl(0, 0%, 15%)" }));
     let audioArray = [];
     useComponentWillMount(() => {
         document.addEventListener('keydown', (event) => {
@@ -16,50 +14,32 @@ const App = () => {
             }
         });
     }, []);
+    useEffect(() => {
+        for (let i = 0; i < Object.keys(soundFiles).length; i++) {
+            document.getElementsByClassName("clip")[i].volume = 0.5;
+        }
+    });
     for (let i = 0; i < Object.keys(soundFiles).length; i++) {
-        audioArray.push(<div id={"button" + i} style={style[i]} className="col-3 drum-pad" onClick={() => {
-            for (let i = 0; i < Object.keys(soundFiles).length; i++) {
-                document.getElementsByClassName("clip")[i].volume = volume / 100;
-            }
+        audioArray.push(<div id={"button" + i} style={style[i]} className="drum-pad" onClick={() => {
             document.getElementById(Object.keys(soundFiles)[i]).play();
-            style[i] = ({ "backgroundColor": "black" })
+            const newStyle = style.slice()
+            newStyle[i] = ({ "backgroundColor": "black" });
+            setStyle(newStyle);
             setTimeout(() => {
-                style[i] = ({ "backgroundColor": "hsl(0, 0%, 15%)" });
-            }, 0);
+                const newStyle = style.slice()
+                newStyle[i] = ({ "backgroundColor": "hsl(0, 0%, 15%)" });
+                setStyle(newStyle);
+            }, 250);
             setSound(soundFiles[Object.keys(soundFiles)[i]].id);
-        }}><audio src={soundFiles[Object.keys(soundFiles)[i]].url} className="clip" id={Object.keys(soundFiles)[i]} key={Object.keys(soundFiles)[i]}/>{Object.keys(soundFiles)[i]}</div>);
+        }}><audio src={soundFiles[Object.keys(soundFiles)[i]].url} className="clip" id={Object.keys(soundFiles)[i]} key={Object.keys(soundFiles)[i]} />{Object.keys(soundFiles)[i]}</div>);
     }
     return (
         <div className="container-fluid vh-100 w-100" id="drum-machine">
-            <div className="col-12">
-                <div className="col-9 row">
-                    {audioArray.slice(0, 3)}
-                </div>
-                <div className="col-9 row">
-                    {audioArray.slice(3, 6)}
-                </div>
-                <div className="col-9 row">
-                    {audioArray.slice(6, 9)}
-                </div>
+            <div id="audio-grid">
+                {audioArray}
             </div>
             <div id="display">
                 {sound}
-            </div>
-            <div id="settings" className="col-md-2 bg-white">
-                <div id="power-btn">
-                    <label id="power-switch">
-                        <input type="checkbox" onClick={() => setPower(!power)} />
-                        <span id="power-slider" />
-                    </label>
-                </div>
-                <div id="volume-slider-cont">
-                    <div id="volume-slider">
-                        <input type="range" min="0" max="100" value={volume} onInput={(event) => { setVolume(event.target.value) }} />
-                        console
-                        <progress min="0" max="100" value={volume} />
-                    </div>
-                    <div id="volume-value">{volume}</div>
-                </div>
             </div>
         </div>
     )
