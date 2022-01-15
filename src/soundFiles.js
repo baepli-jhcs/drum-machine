7f959019e9f63c54582a5a8b09e774181f1c66dc
Ben Aepli <baepli@jhcschool.org>
2022-01-15 14:16:47 -0700

    need to add styling

---
+++ src/soundFiles.js
@@ -0,0 +1,39 @@
+const soundFiles = {
+    Q: {
+        id: 'Heater-1',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    },
+    W: {
+        id: 'Heater-2',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    },
+    E: {
+        id: 'Heater-3',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    },
+    A: {
+        id: 'Heater-4',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    },
+    S: {
+        id: 'Clap',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    },
+    D: {
+        id: 'Open-HH',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    },
+    Z: {
+        id: "Kick-n'-Hat",
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    },
+    X: {
+        id: 'Kick',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    },
+    C: {
+        id: 'Closed-HH',
+        url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    }
+};
+export default soundFiles;
